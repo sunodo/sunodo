181f92f48787b749ac336f3bb390d69591e3e657
Danilo Tuler <tuler@pobox.com>
2023-06-06 18:23:13 -0300

    fix: wrong redis stream name

--- packages/rollups-node/s6-overlay/s6-rc.d/stream-cleanup/script/up.sh
+++ packages/rollups-node/s6-overlay/s6-rc.d/stream-cleanup/script/up.sh
@@ -3,6 +3,6 @@
 if [ ! -z ${ENABLE_STREAM_CLEANUP+x} ];then
     printf "stream-cleanup: ENABLE_STREAM_CLEANUP is set, cleaning redis streams...\n"
     redis-cli -h ${REDIS_ENDPOINT} DEL chain-${CHAIN_ID}:dapp-${DAPP_CONTRACT_ADDRESS:2}:rollups-claims;
-    redis-cli -h ${REDIS_ENDPOINT} DEL chain-${CHAIN_ID}:dapp-${DAPP_CONTRACT_ADDRESS:2}:rollups-input;
+    redis-cli -h ${REDIS_ENDPOINT} DEL chain-${CHAIN_ID}:dapp-${DAPP_CONTRACT_ADDRESS:2}:rollups-inputs;
     redis-cli -h ${REDIS_ENDPOINT} DEL chain-${CHAIN_ID}:dapp-${DAPP_CONTRACT_ADDRESS:2}:rollups-outputs;
 fi
