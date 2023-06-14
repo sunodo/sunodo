099ba60b47c8c3d8026789591a756864ad34dbb1
Danilo Tuler <tuler@pobox.com>
2023-06-14 01:31:14 -0300

    feat(contracts): deploy-anvil hardhat task

---
+++ packages/contracts/src/tasks/deploy-anvil.ts
@@ -0,0 +1,58 @@
+import fs from "fs";
+import path from "path";
+import { task, types } from "hardhat/config";
+import { spawn } from "child_process";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+export interface DeployOptions {
+    dumpFile: string;
+    exportFile?: string;
+    silent: boolean;
+}
+
+const deploy = async (
+    taskArgs: DeployOptions,
+    hre: HardhatRuntimeEnvironment
+) => {
+    const { dumpFile, silent } = taskArgs;
+    const exportFile =
+        taskArgs.exportFile ?? `export/abi/${hre.network.name}.json`;
+
+    // make sure directories exist
+    fs.mkdirSync(path.dirname(dumpFile), { recursive: true });
+    fs.mkdirSync(path.dirname(exportFile), { recursive: true });
+
+    // run anvil
+    const anvil = spawn("anvil", ["--dump-state", dumpFile], {
+        stdio: silent ? "pipe" : "inherit",
+    });
+
+    try {
+        // run deployment
+        console.log(`deploying to anvil and dumping state to ${dumpFile}`);
+        await hre.run("deploy", {
+            export: exportFile,
+            reset: true,
+            silent,
+        });
+    } finally {
+        // kill anvil
+        anvil.kill();
+    }
+};
+
+task("deploy-anvil", "Deploys to anvil and dump state")
+    .addParam<string>(
+        "dumpFile",
+        "anvil state dump file",
+        "build/anvil_state.json",
+        types.string
+    )
+    .addOptionalParam<string>(
+        "exportFile",
+        "hardhat-deploy export file",
+        undefined,
+        types.string
+    )
+    .addFlag("silent", "do not print any log")
+    .setAction(deploy);
