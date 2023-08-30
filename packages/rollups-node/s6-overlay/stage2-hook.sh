2e84aa81ca622aad63724d62b82752b0ef9cefe3
oap75 <omid.asadpoor36@gmail.com>
2023-08-30 14:49:00 +0300

    feat: add host-runner to rollups-node docker image

---
+++ packages/rollups-node/s6-overlay/stage2-hook.sh
@@ -0,0 +1,19 @@
+#!/command/with-contenv sh
+
+# check whether to use host-runner or not
+if [ ! -z ${ENABLE_SUNODO_NODE+x} ]; then
+    /command/s6-echo "host-runner: ENABLE_SUNODO_NODE is set, running in host mode..."
+    # add host-runner service and dependencies of the other services
+    touch /etc/s6-overlay/s6-rc.d/user/contents.d/host-runner
+    touch /etc/s6-overlay/s6-rc.d/inspect-server/dependencies.d/host-runner
+    touch /etc/s6-overlay/s6-rc.d/advance-runner/dependencies.d/host-runner
+    # remove server-manager and dependencies of the other services
+    rm /etc/s6-overlay/s6-rc.d/user/contents.d/server-manager
+    rm /etc/s6-overlay/s6-rc.d/inspect-server/dependencies.d/server-manager
+    rm /etc/s6-overlay/s6-rc.d/advance-runner/dependencies.d/server-manager
+    sed -i '/server-manager/d' /usr/local/bin/is_ready
+else
+    /command/s6-echo "host-runner: ENABLE_SUNODO_NODE is not set, skip the host mode..."
+    sed -i '/host-runner/d' /usr/local/bin/is_ready
+    rm /etc/traefik/conf.d/host-runner.yaml
+fi
