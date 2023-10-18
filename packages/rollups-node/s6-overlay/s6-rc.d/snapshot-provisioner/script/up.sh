a2a2a929942340fc683b5c08e8103599ddfcea73
Enderson Maia <endersonmaia@gmail.com>
2023-10-18 16:03:27 +0100

    feat(rollups-node): supress messages from validator services

--- packages/rollups-node/s6-overlay/s6-rc.d/snapshot-provisioner/script/up.sh
+++ packages/rollups-node/s6-overlay/s6-rc.d/snapshot-provisioner/script/up.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 set -e
 
-printf "Provisioning cartesi-machine snapshot...\n"
 cp --recursive /tmp/machine-snapshots/0_0/* /var/opt/cartesi/machine-snapshots/0_0/
 chown --recursive cartesi:cartesi /var/opt/cartesi/machine-snapshots
