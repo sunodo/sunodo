7a0d99fe56c6137432c890e9d5a6601db953eedf
Enderson Maia <endersonmaia@gmail.com>
2023-11-28 16:14:44 +0000

    fix(rollups-node): stream-cleanup service

--- packages/rollups-node/s6-overlay/stage2-hook.sh
+++ packages/rollups-node/s6-overlay/stage2-hook.sh
@@ -3,8 +3,7 @@ set -e
 
 # check whether to use host-runner or not
 if [ ! -z ${ENABLE_SUNODO_NODE+x} ]; then
-    # add host-runner service and dependencies of the other services
-    touch /etc/s6-overlay/s6-rc.d/user/contents.d/host-runner
+    # add host-runner as dependency the other services
     touch /etc/s6-overlay/s6-rc.d/inspect-server/dependencies.d/host-runner
     touch /etc/s6-overlay/s6-rc.d/advance-runner/dependencies.d/host-runner
     # remove server-manager and dependencies of the other services
@@ -15,4 +14,12 @@ if [ ! -z ${ENABLE_SUNODO_NODE+x} ]; then
     sed -i '/server-manager/d' /usr/local/bin/is_ready
 else
     sed -i '/host-runner/d' /usr/local/bin/is_ready
+fi
+
+# check whether to use stream-cleanup or not
+if [ ! -z ${ENABLE_STREAM_CLEANUP+x} ];then
+    touch /etc/s6-overlay/s6-rc.d/advance-runner/dependencies.d/stream-cleanup
+    touch /etc/s6-overlay/s6-rc.d/authority-claimer/dependencies.d/stream-cleanup
+    touch /etc/s6-overlay/s6-rc.d/dispatcher/dependencies.d/stream-cleanup
+    touch /etc/s6-overlay/s6-rc.d/indexer/dependencies.d/stream-cleanup
 fi
