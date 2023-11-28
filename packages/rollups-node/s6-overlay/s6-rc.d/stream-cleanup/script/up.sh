856fa28dc7fc193dcd89b32d252d820f8aac52fe
Enderson Maia <endersonmaia@gmail.com>
2023-11-28 16:14:44 +0000

    fix(rollups-node): stream-cleanup service

--- packages/rollups-node/s6-overlay/s6-rc.d/stream-cleanup/script/up.sh
+++ packages/rollups-node/s6-overlay/s6-rc.d/stream-cleanup/script/up.sh
@@ -1,8 +1,19 @@
-#!/usr/bin/env sh
+#!/command/with-contenv bash
+set -e
 
-if [ ! -z ${ENABLE_STREAM_CLEANUP+x} ];then
-    printf "stream-cleanup: ENABLE_STREAM_CLEANUP is set, cleaning redis streams...\n"
-    redis-cli -h ${REDIS_ENDPOINT} DEL chain-${CHAIN_ID}:dapp-${DAPP_CONTRACT_ADDRESS:2}:rollups-claims;
-    redis-cli -h ${REDIS_ENDPOINT} DEL chain-${CHAIN_ID}:dapp-${DAPP_CONTRACT_ADDRESS:2}:rollups-inputs;
-    redis-cli -h ${REDIS_ENDPOINT} DEL chain-${CHAIN_ID}:dapp-${DAPP_CONTRACT_ADDRESS:2}:rollups-outputs;
+if [ ! -z ${DAPP_DEPLOYMENT_FILE+x} ]; then
+    _FROM_FILE=$(jq -r '.address | ascii_downcase' ${DAPP_DEPLOYMENT_FILE})
+    DAPP_ADDRESS=${_FROM_FILE}
+else
+    DAPP_ADDRESS=${DAPP_ADDRESS:-}
 fi
+
+REDIS_ARGS="-u ${REDIS_ENDPOINT}"
+[ ! -z ${REDIS_CLUSTER_MODE+x} ] && REDIS_ARGS="${REDIS_ARGS} -c"
+[ ! -z ${REDIS_TLS_MODE:x} ] && REDIS_ARGS="${REDIS_ARGS} --tls"
+
+REDIS_CMD="DEL {chain-${CHAIN_ID}:dapp-${DAPP_ADDRESS:2}}"
+
+redis-cli ${REDIS_ARGS} ${REDIS_CMD}:rollups-claims
+redis-cli ${REDIS_ARGS} ${REDIS_CMD}:rollups-inputs
+redis-cli ${REDIS_ARGS} ${REDIS_CMD}:rollups-outputs
