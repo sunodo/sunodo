5275f61285440e11f2b0c17a6c1c3265a85abe9a
Danilo Tuler <tuler@pobox.com>
2024-03-11 20:55:46 -0300

    feat(cli): deploy command

---
+++ apps/cli/src/commands/deploy/build.ts
@@ -0,0 +1,80 @@
+import { Flags } from "@oclif/core";
+import { execa } from "execa";
+import fs from "fs-extra";
+import path from "path";
+import { tmpNameSync } from "tmp";
+import { URL } from "url";
+
+import { SunodoCommand } from "../../sunodoCommand.js";
+
+export default class DeployBuild extends SunodoCommand<typeof DeployBuild> {
+    static summary = "Build deployment Docker image of application.";
+
+    static description =
+        "Package the application in a Docker image ready to be deployed.";
+
+    static examples = ["<%= config.bin %> <%= command.id %>"];
+
+    static flags = {
+        platform: Flags.string({
+            options: ["linux/amd64", "linux/arm64"],
+            summary: "Docker image target platform",
+            description:
+                "Select the target platform for the produced Docker image. It depends on the platform where the application node will be deployed.",
+        }),
+    };
+
+    private async buildRollupsImage(platform?: string) {
+        const buildResult = tmpNameSync();
+        const imagePath = path.join(".sunodo", "image");
+        const binPath = path.join(
+            path.dirname(new URL(import.meta.url).pathname),
+            "..",
+            "..",
+        );
+        const dockerfile = path.join(binPath, "node", "DockerfileDeploy.txt");
+        const args = [
+            "buildx",
+            "build",
+            "-f",
+            dockerfile,
+            "--load",
+            "--iidfile",
+            buildResult,
+            imagePath,
+        ];
+
+        // optional platform, default to not defining, which will build for the host platform
+        if (platform) {
+            args.push("--platform", platform);
+        }
+
+        await execa("docker", args, { stdio: "inherit" });
+        return fs.readFileSync(buildResult, "utf8");
+    }
+
+    public async run(): Promise<string> {
+        const { flags } = await this.parse(DeployBuild);
+
+        // print machine hash
+        const templateHash = this.getMachineHash();
+        if (!templateHash) {
+            this.error(
+                "Cartesi machine snapshot not found, run 'sunodo build'",
+            );
+        }
+        this.logPrompt({
+            title: "Cartesi machine templateHash",
+            value: templateHash,
+        });
+        this.log("Building application node Docker image...");
+
+        const image = await this.buildRollupsImage(flags.platform);
+        this.logPrompt({
+            title: "Application node Docker image",
+            value: image,
+        });
+
+        return image;
+    }
+}
---
+++ apps/cli/src/commands/deploy/index.ts
@@ -0,0 +1,96 @@
+import { confirm, select } from "@inquirer/prompts";
+import { Flags } from "@oclif/core";
+import chalk from "chalk";
+import open, { apps } from "open";
+import { URL } from "url";
+
+import { SunodoCommand } from "../../sunodoCommand.js";
+
+export default class Deploy extends SunodoCommand<typeof Deploy> {
+    static summary = "Deploy application to a live network.";
+
+    static description =
+        "Package and deploy the application to a supported live network.";
+
+    static examples = ["<%= config.bin %> <%= command.id %>"];
+
+    static flags = {
+        hosting: Flags.string({
+            options: ["self-hosted", "third-party"] as const,
+            summary: "hosting type",
+            description:
+                "Select wheather the user will host an application node himself, or use a third-party node provider",
+        }),
+        webapp: Flags.url({
+            description: "address of sunodo webapp",
+            default: new URL("https://sunodo.io/deploy"),
+            hidden: true,
+        }),
+    };
+
+    public async run(): Promise<void> {
+        const { flags } = await this.parse(Deploy);
+
+        // print machine hash
+        const templateHash = this.getMachineHash();
+        if (!templateHash) {
+            this.error(
+                "Cartesi machine snapshot not found, run 'sunodo build'",
+            );
+        }
+        this.logPrompt({
+            title: "Cartesi machine templateHash",
+            value: templateHash,
+        });
+
+        // ask for deployment type
+        const hosting =
+            flags.hosting ||
+            (await select<"self-hosted" | "third-party">({
+                message: "Select hosting type",
+                choices: [
+                    {
+                        name: "Self-hosting",
+                        description: `Select this option if you want to run the node for your application.
+You will need the following infrastructure:
+
+- a cloud server for the application node
+- a postgres database
+- a web3 node provider
+- a funded wallet
+`,
+                        value: "self-hosted",
+                    },
+                    {
+                        name: "Use third-party provider",
+                        description:
+                            "Select this option to use a third-party service provider to run a node for your application.",
+                        value: "third-party",
+                        disabled: "(coming soon)",
+                    },
+                ],
+            }));
+
+        let queryString = "";
+        switch (hosting) {
+            case "self-hosted": {
+                // build docker image
+                await this.config.runCommand("deploy:build");
+
+                queryString = `?templateHash=${templateHash}`;
+                break;
+            }
+            case "third-party": {
+                this.error("Third-party provider deployment not supported yet");
+            }
+        }
+
+        // prompt user to open webapp for onchain deployment
+        const deployUrl = `${flags.webapp}${queryString}`;
+        if (await confirm({ message: `Open ${chalk.cyan(deployUrl)}?` })) {
+            open(deployUrl, { app: { name: apps.chrome } });
+        }
+
+        return;
+    }
+}
