985bf9f59affa03da28c0241cecfaa81a35b89d0
Danilo Tuler <tuler@pobox.com>
2024-03-14 11:35:47 -0300

    feat(web): storybook

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     root: true,
-    extends: ["@sunodo/eslint-config/next.js"],
+    extends: ["@sunodo/eslint-config/next.js", "plugin:storybook/recommended"],
     parser: "@typescript-eslint/parser",
     parserOptions: {
         project: true,
