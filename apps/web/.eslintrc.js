5e857d9f6796cab1e34f2bf10ab6d2a8d7518212
Danilo Tuler <tuler@pobox.com>
2024-03-14 17:21:28 -0300

    feat(web): add @typescript-eslint/recommended to eslint config

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
     root: true,
-    extends: ["@sunodo/eslint-config/next.js", "plugin:storybook/recommended"],
+    extends: [
+        "@sunodo/eslint-config/next.js",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:storybook/recommended",
+    ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
         project: true,
