e11356c8a7da77b5f4d46986d21fdd025d769571
Danilo Tuler <tuler@pobox.com>
2024-03-14 11:35:27 -0300

    feat(web): wallet

---
+++ apps/web/providers/walletProvider.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useMantineColorScheme } from "@mantine/core";
+import {
+    RainbowKitProvider,
+    darkTheme,
+    getDefaultConfig,
+    lightTheme,
+} from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+    arbitrum,
+    arbitrumSepolia,
+    foundry,
+    mainnet,
+    optimism,
+    optimismSepolia,
+    sepolia,
+} from "viem/chains";
+import { WagmiProvider } from "wagmi";
+
+const projectId = "bc37f90dfcefc2900e5a86d366bf9aea";
+const queryClient = new QueryClient();
+
+const config = getDefaultConfig({
+    appName: "Sunodo",
+    projectId,
+    chains:
+        process.env.NODE_ENV === "development"
+            ? [
+                  mainnet,
+                  sepolia,
+                  arbitrum,
+                  arbitrumSepolia,
+                  optimism,
+                  optimismSepolia,
+                  foundry,
+              ]
+            : [
+                  mainnet,
+                  sepolia,
+                  arbitrum,
+                  arbitrumSepolia,
+                  optimism,
+                  optimismSepolia,
+              ],
+    ssr: true,
+});
+
+const WalletProvider = ({ children }: { children: React.ReactNode }) => {
+    const scheme = useMantineColorScheme();
+    const walletTheme =
+        scheme.colorScheme == "dark" ? darkTheme() : lightTheme();
+
+    return (
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                <RainbowKitProvider theme={walletTheme}>
+                    {children}
+                </RainbowKitProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+    );
+};
+
+export default WalletProvider;
