e305cd596ca7dd1fa208fbb16228f6f53c3f252b
Danilo Tuler <tuler@pobox.com>
2024-04-04 17:37:32 -0300

    feat(token): add test ERC721

---
+++ packages/token/contracts/SunodoNFT.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: Apache-2.0
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact admin@sunodo.io
+contract SunodoNFT is ERC721, ERC721URIStorage, Ownable {
+    constructor(
+        address initialOwner
+    ) ERC721("SunodoNFT", "SUNN") Ownable(initialOwner) {}
+
+    function safeMint(
+        address to,
+        uint256 tokenId,
+        string memory uri
+    ) public onlyOwner {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
