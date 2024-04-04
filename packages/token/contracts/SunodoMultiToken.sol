07ce28ad25a1574cc2395dcef404e22331f8c3ca
Danilo Tuler <tuler@pobox.com>
2024-04-04 17:37:45 -0300

    feat(token): add test ERC1155

---
+++ packages/token/contracts/SunodoMultiToken.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: Apache-2.0
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact admin@sunodo.io
+contract SunodoMultiToken is ERC1155, Ownable {
+    constructor(address initialOwner) ERC1155("") Ownable(initialOwner) {}
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public onlyOwner {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public onlyOwner {
+        _mintBatch(to, ids, amounts, data);
+    }
+}
