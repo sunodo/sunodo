6284874039d32907fd34baeb53b34917777b4dc2
Danilo Tuler <tuler@pobox.com>
2024-04-22 12:41:35 -0400

    feat: delete website

--- apps/web/components/BelowHeaderWrapper/BelowHeaderWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import classes from "./BelowHeaderWrapper.module.css";
-
-type Props = {
-    children: React.ReactNode;
-};
-
-export function BelowHeaderWrapper({ children }: Props) {
-    return <div className={classes.wrapper}>{children}</div>;
-}
--- apps/web/components/Content/Content.tsx
+++
@@ -1,9 +0,0 @@
-import classes from "./Content.module.css";
-
-type Props = {
-    children: React.ReactNode;
-};
-
-export function Content({ children }: Props) {
-    return <div className={classes.wrapper}>{children}</div>;
-}
