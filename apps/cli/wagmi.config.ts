459ab030d105c27acd088b88710924d217fc2162
Danilo Tuler <tuler@pobox.com>
2024-04-22 17:21:38 -0400

    feat(cli): use latest devnet (renamed tokens)

--- apps/cli/wagmi.config.ts
+++ apps/cli/wagmi.config.ts
@@ -1,14 +1,9 @@
 import hardhatDeploy from "@sunodo/wagmi-plugin-hardhat-deploy";
 import { defineConfig } from "@wagmi/cli";
-import { hardhat } from "@wagmi/cli/plugins";
 
 export default defineConfig({
     out: "src/contracts.ts",
     plugins: [
-        hardhat({
-            project: "../../packages/contracts",
-            include: [],
-        }),
-        hardhatDeploy({ directory: "../../packages/devnet/export/abi" }),
+        hardhatDeploy({ directory: "node_modules/@sunodo/devnet/export/abi" }),
     ],
 });
