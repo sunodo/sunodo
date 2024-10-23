128bc15d5450040004a5ed43650e70a525630113
Danilo Tuler <tuler@pobox.com>
2024-10-22 20:02:27 -0400

    feat(web): bump dependencies

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <head>
                 <ColorSchemeScript defaultColorScheme="light" />
                 <link rel="shortcut icon" href="/favicon.svg" />
