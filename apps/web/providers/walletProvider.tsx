f7ee900cd7f2c2e7684d2043e81ae8db2fc4ecb5
Danilo Tuler <tuler@pobox.com>
2025-03-14 14:59:16 -0400

    feat(web): alchemy rpc provider

--- apps/web/providers/walletProvider.tsx
+++ apps/web/providers/walletProvider.tsx
@@ -2,10 +2,10 @@
 
 import { useMantineColorScheme } from "@mantine/core";
 import {
-    RainbowKitProvider,
     darkTheme,
     getDefaultConfig,
     lightTheme,
+    RainbowKitProvider,
 } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -20,9 +20,10 @@ import {
     optimismSepolia,
     sepolia,
 } from "viem/chains";
-import { WagmiProvider } from "wagmi";
+import { http, WagmiProvider } from "wagmi";
 
 const projectId = "bc37f90dfcefc2900e5a86d366bf9aea";
+const alchemyApiKey = process.env.ALCHEMY_API_KEY;
 const queryClient = new QueryClient();
 
 const config = getDefaultConfig({
@@ -51,6 +52,33 @@ const config = getDefaultConfig({
                   base,
                   baseSepolia,
               ],
+    transports: {
+        [mainnet.id]: http(
+            `https://eth-mainnet.g.alchemy.com/v2/${alchemyApiKey}`,
+        ),
+        [sepolia.id]: http(
+            `https://eth-sepolia.g.alchemy.com/v2/${alchemyApiKey}`,
+        ),
+        [arbitrum.id]: http(
+            `https://arb-mainnet.g.alchemy.com/v2/${alchemyApiKey}`,
+        ),
+        [arbitrumSepolia.id]: http(
+            `https://arb-sepolia.g.alchemy.com/v2/${alchemyApiKey}`,
+        ),
+        [optimism.id]: http(
+            `https://opt-mainnet.g.alchemy.com/v2/${alchemyApiKey}`,
+        ),
+        [optimismSepolia.id]: http(
+            `https://opt-sepolia.g.alchemy.com/v2/${alchemyApiKey}`,
+        ),
+        [base.id]: http(
+            `https://base-mainnet.g.alchemy.com/v2/${alchemyApiKey}`,
+        ),
+        [baseSepolia.id]: http(
+            `https://base-sepolia.g.alchemy.com/v2/${alchemyApiKey}`,
+        ),
+        [foundry.id]: http(),
+    },
     ssr: true,
 });
 
